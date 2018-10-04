99f3e4f5576b57f67c00b2840f263e4c59455594
valetodo <estalinyerel123@gmail.com>
2018-10-04 15:04:51 -0400

    practice 01 and practice 01

---
+++ yerel hurtado/practice02.py
@@ -0,0 +1,14 @@
+import operator
+
+
+def operation(operator_math, operator_one, operator_two):
+    operation_math = {"+": operator.add, "-": operator.sub, "*": operator.mul, "/": operator.truediv}
+    if operator_math == "+" or operator_math == "*" or operator_math == "-" or operator_math == "+":
+        print(operation_math[operator_math](float(operator_one), float(operator_two)))
+
+
+operation("+", "3", "4")
+operation("-", "3", "4")
+operation("*", "3", "4")
+operation("/", "4", "3")
+operation("5", "4", "5")
