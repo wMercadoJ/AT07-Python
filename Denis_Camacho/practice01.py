ddeeb629444314bb8c6eeca68c05bc9b8f0ec0f9
denis1729 <denis_cc1729@hotmail.com>
2018-10-04 16:03:15 -0400

    my practice 1 and 2

---
+++ Denis_Camacho/practice01.py
@@ -0,0 +1,24 @@
+number_fist_500_data = 500
+number_second_300_data = 3
+# Addition
+addition_number_500_300_result = number_fist_500_data + number_second_300_data
+print ("The addition result is:", addition_number_500_300_result)
+# subtraction
+subtraction_number_500_300_result = number_fist_500_data - number_second_300_data
+print("The subtraction result is:", subtraction_number_500_300_result)
+# multiplication
+multiplication_500_300_result = number_fist_500_data * number_second_300_data
+print ("The multiplication result is:", multiplication_500_300_result)
+# division
+division_500_300_result = number_fist_500_data / number_second_300_data
+print ("The division result is:", division_500_300_result)
+# modulus
+modulus_500_300_result = number_fist_500_data % number_second_300_data
+print ("The modulus result is:", modulus_500_300_result)
+# exponent
+exponent_500_300_result = number_fist_500_data ** number_second_300_data
+print ("The exponent result is:", exponent_500_300_result)
+# Floor Division
+floor_division_500_300_result = number_fist_500_data // number_second_300_data
+print ("The Floor Division result is:", floor_division_500_300_result)
+
