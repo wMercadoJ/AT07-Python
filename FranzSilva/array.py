939ce34c2e3c52b51387582f4690d4bd767a9478
franz.silva@fundacion-jala.org <franz.silva@fundacion-jala.org>
2018-10-08 10:23:36 -0400

    change i for index on arrays.py

--- FranzSilva/array.py
+++ FranzSilva/array.py
@@ -2,10 +2,10 @@
 def create_array():
     array = []
     length = int(input('Insert the length of array:'))
-    for i in range(length):
+    for index in range(length):
         value = input('Insert the values:')
         array.append(value)
     return array
 
 
-create_array(create_array())
+print(create_array())
